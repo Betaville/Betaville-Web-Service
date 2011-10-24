d9c7fac5924efaa0389d699e7355ae2e889e188c
Skye Book <skye.book@gmail.com>
2011-10-24 15:02:01 -0400

    Removed session creation from the user database file

--- inc/class.user.inc.php
+++ inc/class.user.inc.php
@@ -91,28 +91,6 @@ class UserActions{
 		return @mail($to,$subject,$message,$headers);
 	}
 	
-	private function createToken($username){
-		session_start();
-		
-		// If the tokens array does not already exist, create it
-		if(!isset($_SESSION['tokens'])){
-			$tokensArray = array();
-			$_SESSION['tokens'] = $tokensArray;
-		}
-		
-		// create the hash that we will use and check that it is unique
-		$hash = SHA1($this->createSalt().$username);
-		while($this->checkForTokenMatch($hash)){
-			$hash = SHA1($this->createSalt().$username);
-		}
-		
-		// we now have a unique hash, let's create the token
-		$newToken = array('hash'=>$hash, 'username'=>$username, 'time'=>time());
-		$_SESSION['tokens'][] = $newToken;
-		
-		return $hash;
-	}
-	
 	private function checkForTokenMatch($hash){
 		foreach($_SESSION['tokens'] as &$token){
 				if($token['hash'] == $hash) return true;
@@ -169,9 +147,8 @@ class UserActions{
 	}
 	
 	public function login($username, $password){
-	
 		$authResult = $this->authenticate($username, $password);
-		if($authResult) return array('authenticationSuccess'=>true, 'token'=>$this->createToken($username));
+		if($authResult) return array('authenticationSuccess'=>true);
 		else return array('authenticationSuccess'=>false);
 	
 		$hashSQL = "SELECT username, strongpass, strongsalt from user where username=:user LIMIT 1";
