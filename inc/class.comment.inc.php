738f6b3a2c106de575089379f28dd34eb38d9f92
Skye Book <skye.book@gmail.com>
2011-11-08 13:09:12 -0500

    fix for some confusion in comment queries

--- inc/class.comment.inc.php
+++ inc/class.comment.inc.php
@@ -64,7 +64,7 @@ class CommentActions{
 			while($row=$stmt->fetch()){
 				// only include this result if it hasn't been verified as spam
 				if($row[COMMENT_SPAMVERIFIED]==0){
-					$comments[] = $this->commentFromRow($row);
+					$comments[] = $this->commentFromRow($row, COMMENT_USER);
 				}
 			}
 			return $comments;
@@ -86,7 +86,7 @@ class CommentActions{
 			while($row=$stmt->fetch()){
 				// only include this result if it hasn't been verified as spam
 				if($row[COMMENT_SPAMVERIFIED]==0){
-					$comments[] = $this->commentFromRow($row);
+					$comments[] = $this->commentFromRow($row, COMMENT_USER);
 				}
 			}
 			return $comments;
@@ -98,22 +98,19 @@ class CommentActions{
 	}
 	
 	public function getNotificationsForUser($username){
-		// select * from comment join design on comment.designid = design.designid where design.isalive=1 AND (design.favelist like '%sbook%' OR design.user like 'sbook' OR comment.user like 'sbook');
-		/**$sql = 'SELECT * FROM '.COMMENT_TABLE.' JOIN '.DESIGN_TABLE.' ON '.COMMENT_TABLE.'.'.COMMENT_DESIGN.' = '.DESIGN_TABLE.'.'.DESIGN_ID.' WHERE '.DESIGN_TABLE.'.'.DESIGN_IS_ALIVE.'=1 AND 				('.DESIGN_TABLE.'.'.DESIGN_FAVE_LIST.' LIKE :wildcardname OR '.DESIGN_TABLE.'.'.DESIGN_USER.' LIKE :username OR '.COMMENT_TABLE.'.'.COMMENT_USER.' LIKE :username)';
-		$wildcardName = '%'.$username.'%';*/
-		$sql = 'SELECT '.COMMENT_TABLE.'.'.COMMENT_ID.' , '.COMMENT_TABLE.'.'.COMMENT_DESIGN.' , '.DESIGN_TABLE.'.'.DESIGN_NAME.' , '.DESIGN_TABLE.'.'.DESIGN_FILE.' , '.COMMENT_TABLE.'.'.COMMENT_USER.' , '.COMMENT_TABLE.'.'.COMMENT_TEXT.' , '.
-COMMENT_TABLE.'.'.COMMENT_DATE.' , '.COMMENT_TABLE.'.'.COMMENT_REPLIESTO.' FROM '.COMMENT_TABLE.' JOIN '.DESIGN_TABLE.' ON '.COMMENT_TABLE.'.'.COMMENT_DESIGN.' = '.DESIGN_TABLE.'.'.DESIGN_ID.' AND '.COMMENT_TABLE.'.'.COMMENT_USER.' = "'.$username.'" ORDER BY commentid DESC';
+		$sql = 'SELECT *, '.COMMENT_TABLE.'.'.COMMENT_USER.' AS commentuser, '.DESIGN_TABLE.'.'.DESIGN_USER.' AS designuser FROM '.COMMENT_TABLE.' JOIN '.DESIGN_TABLE.' ON '.COMMENT_TABLE.'.'.COMMENT_DESIGN.' = '.DESIGN_TABLE.'.'.DESIGN_ID.' WHERE '.DESIGN_TABLE.'.'.DESIGN_IS_ALIVE.'=1 AND ('.DESIGN_TABLE.'.'.DESIGN_FAVE_LIST.' LIKE :wildcardname OR '.DESIGN_TABLE.'.'.DESIGN_USER.' LIKE :username OR '.COMMENT_TABLE.'.'.COMMENT_USER.' LIKE :username) ORDER BY '.COMMENT_ID.' DESC';
+		$wildcardName = '%'.$username.'%';
 			try{
 			$stmt = $this->_db->prepare($sql);
-			//$stmt->bindParam(":wildcardname", $wildcardName, PDO::PARAM_STR);
-			//$stmt->bindParam(":username", $username, PDO::PARAM_STR);
+			$stmt->bindParam(":wildcardname", $wildcardName, PDO::PARAM_STR);
+			$stmt->bindParam(":username", $username, PDO::PARAM_STR);
 			$stmt->execute();
 			$comments = array();
 			while($row=$stmt->fetch()){
 				// only include this result if it hasn't been verified as spam
-				//if($row[COMMENT_SPAMVERIFIED]==0){
-					$comments[] = $this->commentFromSpecificRow($row);
-				//}
+				if($row[COMMENT_SPAMVERIFIED]==0){
+					$comments[] = $this->commentFromRow($row, 'commentuser');
+				}
 			}
 			return $comments;
 		}catch(PDOException $e){
@@ -126,8 +123,8 @@ COMMENT_TABLE.'.'.COMMENT_DATE.' , '.COMMENT_TABLE.'.'.COMMENT_REPLIESTO.' FROM 
 	private function commentFromSpecificRow($row) {
 		return array(COMMENT_ID=>$row[COMMENT_ID], COMMENT_DESIGN=>$row[COMMENT_DESIGN], DESIGN_NAME=>$row[DESIGN_NAME], DESIGN_FILE=>$row[DESIGN_FILE], COMMENT_USER=>$row[COMMENT_USER], COMMENT_TEXT=>$row[COMMENT_TEXT], COMMENT_DATE=>$row[COMMENT_DATE], COMMENT_REPLIESTO=>$row[COMMENT_REPLIESTO]);
 }
-	private function commentFromRow($row){
-		return array(COMMENT_ID=>$row[COMMENT_ID], COMMENT_DESIGN=>$row[COMMENT_DESIGN], COMMENT_USER=>$row[COMMENT_USER],COMMENT_TEXT=>$row[COMMENT_TEXT],
+	private function commentFromRow($row, $commentUserAlias){
+		return array(COMMENT_ID=>$row[COMMENT_ID], COMMENT_DESIGN=>$row[COMMENT_DESIGN], COMMENT_USER=>$row[$commentUserAlias],COMMENT_TEXT=>$row[COMMENT_TEXT],
 		COMMENT_DATE=>$row[COMMENT_DATE],COMMENT_REPLIESTO=>$row[COMMENT_REPLIESTO]);
 	}
 }
